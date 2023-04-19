ef72cdd04e402512b7480f1e39c63aaa0d224ee2
mohammed-algh <mohammedhalgh@hotmail.com>
2023-04-19 16:01:48 +0300

    Fix some issues #2

--- Website.py
+++ Website.py
@@ -7,9 +7,15 @@ def analyze(link:str, classifier:str):
     except Exception as e:
         st.write(f"{e}")
 
-
+st.image("Wasi Logo.png", width=200)
 st.title("WASI | Arabic Youtube Recommender")
+
 st.markdown("---")
 link = st.text_input("Enter Youtube Link Here", placeholder="E.g. https://www.youtube.com")
-radio = st.radio("Choose Classifier:", options=("Naive Bayes (Recommended)","SVM","Random Forest","Decision Tree","KNN", "Logistic Regression"), )
+
+with st.expander("Advance Settings"):
+    radio = st.radio("Choose Classifier:", options=("Naive Bayes (Recommended)","SVM","Random Forest","Decision Tree","KNN", "Logistic Regression"), )
+
 btn_analyze = st.button("Analyze",on_click=analyze(link,radio))
+
+
