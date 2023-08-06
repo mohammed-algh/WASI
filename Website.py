07802990601fcd388af66f763c622368ef8c418e
ikema77 <brhoom.kh@gmail.com>
2023-08-06 19:13:46 +0300

    Update colors

--- Website.py
+++ Website.py
@@ -187,7 +187,7 @@ def wasi():
                 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.egzxvld5 > div.block-container.css-z5fcl4.egzxvld4 > div:nth-child(1) > div > div:nth-child(7) > div > button
                 { background:#f44336; color:white;}
                 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.egzxvld5 > div.block-container.css-z5fcl4.egzxvld4 > div:nth-child(1) > div > div.css-12ttj6m.epcbefy1 > div:nth-child(1) > div > div:nth-child(5) > div > div > button
-                { background:#ddd; color:black;}
+                { background:#eee; color:black;}
                 #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.css-uf99v8.egzxvld5 > div.block-container.css-z5fcl4.egzxvld4 > div:nth-child(1) > div > div.css-12ttj6m.epcbefy1 > div:nth-child(1) > div > div:nth-child(5) > div > div > button:hover
                 { background:white; color:red;}
         </style>
